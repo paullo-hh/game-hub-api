22d56a6820136184668c40eebed02e90f3940c80
Paulo H. Sousa <contacthenry96@gmail.com>
2025-08-18 16:58:45 -0300

    First commit \o/

---
+++ src/main/java/org/carvalho/gamehubapi/GameHubApiApplication.java
@@ -0,0 +1,13 @@
+package org.carvalho.gamehubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHubApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameHubApiApplication.class, args);
+  }
+
+}
